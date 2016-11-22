a8a3547c9684298c633ee4aa5e140bdc4ed30aa3
Adrián García <adrian.garcia@bq.com>
2016-11-22 14:55:03 +0100

    Improve photo saving permformance by inserting MediaStore values directly instead of using insertimage

--- app/src/main/java/com/bq/daggerskeleton/sample/storage/StorageStore.java
+++ app/src/main/java/com/bq/daggerskeleton/sample/storage/StorageStore.java
@@ -2,6 +2,9 @@ package com.bq.daggerskeleton.sample.storage;
 
 import android.app.Application;
 import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 
@@ -13,7 +16,6 @@ import com.bq.daggerskeleton.sample.photo.PhotoAvailableAction;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -108,10 +110,26 @@ public class StorageStore extends Store<StorageState> {
    }
 
    private String addImageToMediaStore(File fileToAdd) {
-      try {
-         return MediaStore.Images.Media.insertImage(contentResolver, fileToAdd.getAbsolutePath(), fileToAdd.getName(), null);
-      } catch (FileNotFoundException e) {
-         Timber.e(e, "Could not add file %s to MediaStore", fileToAdd.getAbsoluteFile());
+      // Decode bitmap bounds to insert them in MediaStore
+      BitmapFactory.Options options = new BitmapFactory.Options();
+      options.inJustDecodeBounds = true;
+      BitmapFactory.decodeFile(fileToAdd.getAbsolutePath(), options);
+
+      // Insert data to MediaStore database
+      ContentValues values = new ContentValues();
+      values.put(MediaStore.Images.Media.DATA, fileToAdd.getAbsolutePath());
+      values.put(MediaStore.Images.Media.DISPLAY_NAME, fileToAdd.getName());
+      values.put(MediaStore.Images.Media.DATE_TAKEN, fileToAdd.lastModified());
+      values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+      values.put(MediaStore.Images.Media.HEIGHT, options.outHeight);
+      values.put(MediaStore.Images.Media.WIDTH, options.outWidth);
+
+      Uri fileUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+      if (fileUri != null) {
+         return fileUri.toString();
+      } else {
+         Timber.e("Could not add file %s to MediaStore", fileToAdd.getAbsoluteFile());
          return null;
       }
    }
